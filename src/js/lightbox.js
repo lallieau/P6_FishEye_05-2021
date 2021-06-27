74944d2257f6a0d09117915407b98c26e8c893d1
Lallie <lallie.audry@gmail.com>
2021-06-27 12:36:23 +0200

    continue lightbox

--- src/js/lightbox.js
+++ src/js/lightbox.js
@@ -1,45 +1,46 @@
+const closeLightbox = () => {
+  document.querySelector('.lightbox').classList.add('fadeOut');
+  //document.location.reload();
+};
+
 const lightboxTemplate = () => {
-  const dom = document.createElement('div');
-  dom.classList.add('lightbox');
-  dom.innerHTML = `<button class="lightbox__close">Fermer</button>
+  const template = document.createElement('div');
+  template.classList.add('lightbox');
+  template.innerHTML = `<button class="lightbox__close">Fermer</button>
       <button class="lightbox__next">Suivant</button>
       <button class="lightbox__prev">Précédent</button>
       <div class="lightbox__container">
       </div>`;
-  return dom;
 
-  // attach this somewhere in html document.querySelector().append(elemet)
+  document.querySelector('body').append(template);
+
+  const closeBtn = document.querySelector('.lightbox__close');
+  closeBtn.addEventListener('click', closeLightbox);
 };
-
-const links = document
-  .querySelectorAll('div[data-href$=".png"], div[data-href$=".jpg"], div[data-href$=".jpeg"]')
-  .forEach(link =>
-    link.addEventListener('click', e => {
-      e.preventDefault();
-      // add a lightbox template
-      lightboxTemplate();
-      // add image into our lightbox
-    }),
-  );
 
 const loadImage = url => {
   const image = new Image();
-  const container = this.element.querySelector('.lightbox__container');
-  const loader = document.createElement('div');
-  loader.classList.add('lightbox__loader');
-  container.appendChild.loader;
-  image.onload = function () {
-    container.removeChild(loader);
-    container.appendChild(image);
-  };
+  const container = document.querySelector('.lightbox__container');
   image.src = url;
+  container.append(image);
 };
 
-// 1) lister tous tes images (no link) (probablement devoir rajouter un data-href lors de la création des images afin de récupérer le lien ici)
+const links = Array.from(document.querySelectorAll('div[data-href$=".jpg"]'));
 
-// 2) Lors du clik d'un de ces liens faire apparaitre la lightbox
+links.forEach(link =>
+  link.addEventListener('click', e => {
+    e.preventDefault();
+    lightboxTemplate();
 
-// 3) Ajouter l'image en question à la lightbox (dans le container)
+    const url = link.getAttribute('data-href');
+
+    loadImage(url);
+  }),
+);
+
+// console.log(document.querySelectorAll(image.getAttribute('href')));
+// const gallery = links.map(link => image.getAttribute('href'));
+// console.log(gallery);
 
 // STEP next / previous
 
@@ -48,62 +49,3 @@ const loadImage = url => {
 // Find current position inside the global object
 
 // on next or previous click switch container depending on href  (step 3 of initial todolist)
-
-// class Lightbox {
-//   static init() {
-//     const links = document.querySelectorAll('a[href$=".png"], a[href$=".jpg"], a[href$=".jpeg"]').forEach(link =>
-//       link.addEventListener('click', e => {
-//         e.preventDefault();
-//         new Lightbox(e.currentTarget.getAttribute('href'));
-//       }),
-//     );
-//   }
-
-//   constructor(url) {
-//     this.element = this.BuildDOM(url);
-//     this.loadImage(url);
-//     this.onKeyUp = this.onKeyUp.bind(this);
-//     document.body.appendChild(this.element);
-//     document.addEventListener('keyup', this.onKeyUp);
-//   }
-
-//   loadImage(url) {
-//     const image = new Image();
-//     const container = this.element.querySelector('.lightbox__container');
-//     const loader = document.createElement('div');
-//     loader.classList.add('lightbox__loader');
-//     container.appendChild.loader;
-//     image.onload = function () {
-//       container.removeChild(loader);
-//       container.appendChild(image);
-//     };
-//     image.src = url;
-//   }
-
-//   onKeyUp(e) {
-//     if (e.key === 'Escape') {
-//       this.close(e);
-//     }
-//   }
-
-//   close(e) {
-//     e.preventDefault();
-//     this.element.classList.add('fadeOut');
-//     window.setTimeout(() => {
-//       this.element.parentElement.removeChild(this.element);
-//     }, 500);
-//     document.removeEventListener('keyup', this.onKeyUp);
-//   }
-
-//   BuildDOM(url) {
-//     const dom = document.createElement('div');
-//     dom.classList.add('lightbox');
-//     dom.innerHTML = `<button class="lightbox__close">Fermer</button>
-//     <button class="lightbox__next">Suivant</button>
-//     <button class="lightbox__prev">Précédent</button>
-//     <div class="lightbox__container">
-//     </div>`;
-//     dom.querySelector('.lightbox__close').addEventListener('click', this.close.bind(this));
-//     return dom;
-//   }
-// }
